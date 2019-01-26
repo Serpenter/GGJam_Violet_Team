2a84f0cdbee8171f81d3e10e16ff36eb9e59df16
bonbony <mr.mashaishere@gmail.com>
2019-01-26 21:15:47 +0200

    gd fix

---
+++ Obstacles/Area.gd
@@ -0,0 +1,21 @@
+extends Area
+
+var hp = 3
+
+func _ready():
+	self.connect("area_entered",self,"on_area_entered")
+	add_to_group("destructable_objects")
+
+func _process(delta):
+		
+	if hp <= 0:
+		var parent =  get_parent()
+		var root = parent.get_parent()
+		root.remove_child(parent)
+		parent.call_deferred("free")
+		
+
+func on_area_entered(object):
+	var groups = object.get_groups()
+	if object.is_in_group("bullets"):
+		hp = hp - object.damage
